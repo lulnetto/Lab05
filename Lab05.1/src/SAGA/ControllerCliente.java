0fd2be47cf9d29a740378b57f19ca54c3f2f83e2
lulnetto <50139832+lulnetto@users.noreply.github.com>
2019-05-08 20:58:52 -0300

    Update ControllerCliente.java
    Documentado

--- Lab05.1/src/SAGA/ControllerCliente.java
+++ Lab05.1/src/SAGA/ControllerCliente.java
@@ -2,15 +2,32 @@ package SAGA;
 
 import java.util.TreeMap;
 
+/**
+ * Sistema controlador dos clientes.
+ * @author Lourival Gonçalves Prata Netto - 119111236 - UFCG.
+ *
+ */
 public class ControllerCliente {
 
-    private TreeMap<String, Cliente> clientes;
+    private Map<String, Cliente> clientes;
 
+    /**
+	 * Construtor que inicializa um TreeMap de clientes.
+	 */
     public  ControllerCliente()
     {
         this.clientes = new TreeMap<>();
     }
 
+    /**
+	 * Adiciona um novo cliente valido apartir do seu cpf, nome, email e localizacao.
+	 * 
+	 * @param cpf cpf do cliente.
+	 * @param nome nome do cliente.
+	 * @param email email do cliente.
+	 * @param localizacao localizacao do cliente.
+	 * @return retorna o CPF se o cliente for cadastrado com sucesso.
+	 */
     public String cadastraCliente(String cpf, String nome, String email, String localizacao)
     {
         if (clientes.containsKey(cpf))
@@ -22,6 +39,12 @@ public class ControllerCliente {
         return cpf;
     }
 
+    /**
+	 * Exibe um cliente valido apartir do seu cpf.
+	 * 
+	 * @param cpf cpf do cliente.
+	 * @return uma string que representa o cliente no formato NOME - LOCALIZACAO - EMAIL, ou Cliente não cadastrado. em demais casos.
+	 */
     public String exibeCliente(String cpf)
     {
         if (!this.clientes.containsKey(cpf))
@@ -31,6 +54,11 @@ public class ControllerCliente {
         return this.clientes.get(cpf).toString();
     }
 
+     /**
+	 * Exibe todos clientes cadastrados.
+	 * 
+	 * @return uma string que representa todos os clientes no formato NOME - LOCALIZACAO - EMAIL | NOME - LOCALIZACAO - EMAIL.
+	 */
     public String exibeClientesCadastrados()
     {
         String msg = "";
@@ -47,6 +75,13 @@ public class ControllerCliente {
         }
     }
 
+     /**
+	 * Edita o nome de um cliente valido apartir do seu cpf e o novo nome.
+	 * @param cpf cpf do cliente.
+	 * @param nome novo nome.
+	 * 
+	 * @return retorna um booleando True se a edicao do cliente for um sucesso.
+	 */
     public boolean alteraNomeCliente(String cpf, String nome)
     {
         if (!this.clientes.containsKey(cpf))
@@ -57,6 +92,13 @@ public class ControllerCliente {
         return true;
     }
 
+     /**
+	 * Edita o email de um cliente valido apartir do seu cpf e o novo email.
+	 * @param cpf cpf do cliente.
+	 * @param email novo email.
+	 * 
+	 * @return retorna um booleando True se a edicao do cliente for um sucesso.
+	 */
     public boolean alteraEmailCliente(String cpf, String email)
     {
         if (!this.clientes.containsKey(cpf))
@@ -67,6 +109,13 @@ public class ControllerCliente {
         return true;
     }
 
+     /**
+	 * Edita a localizacao de um cliente valido apartir do seu cpf e a nova localizacao.
+	 * @param cpf cpf do cliente.
+	 * @param localizacao nova localizacao.
+	 * 
+	 * @return retorna um booleando True se a edicao do cliente for um sucesso.
+	 */
     public boolean alteraLocalizacaoCliente(String cpf, String localizacao)
     {
         if (!this.clientes.containsKey(cpf))
@@ -77,6 +126,12 @@ public class ControllerCliente {
         return true;
     }
 
+    /**
+	 * Remove um cliente valido apartir do seu cpf.
+	 * 
+	 * @param cpf cpf do cliente.
+	 * @return retorna um booleano True se a remocao do cliente for um sucesso.
+	 */
     public boolean removeCliente(String cpf)
     {
         if (!this.clientes.containsKey(cpf))
